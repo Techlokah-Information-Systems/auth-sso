d60988dcad305db96f7221f6be3fc111c2c1a73e
ShadowConYT <iamajay6122@gmail.com>
2026-03-11 00:18:43 +0530

    feat: initial commit

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { SignUp, ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
+import { SignUp, ClerkLoading, ClerkLoaded, useSignUp } from "@clerk/nextjs";
 import { Loader } from "@/app/components/loader";
 
 export default function SignUpPage() {
+  const { signUp } = useSignUp();
+
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <ClerkLoading>
